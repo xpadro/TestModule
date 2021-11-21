9bc5aa49efd124734c3e2081a97b6258a98ee3fe
xpadro <xpadro.sw@gmail.com>
2021-11-21 19:10:23 +0100

    Add mongodb requirement comment

--- test-manager-runtime/src/main/java/xpadro/testmanager/runtime/TestModuleApplication.java
+++ test-manager-runtime/src/main/java/xpadro/testmanager/runtime/TestModuleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestModuleApplication {
 
+	// Running the application locally requires a MongoDB server (mongod) listening to port 27017
 	public static void main(String[] args) {
 		SpringApplication.run(TestModuleApplication.class, args);
 	}
